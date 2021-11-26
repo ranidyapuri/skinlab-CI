2de694ae46bf02e711097d0a626ecd3deebd666b
unknown <ranidyaputri02@gmail.com>
2021-11-26 14:27:01 +0700

    mengubah controller User

--- application/controllers/User/Page.php
+++ application/controllers/User/Page.php
@@ -4,8 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Page extends CI_Controller{
     public function index()
     {
+        $data['title'] = 'User';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $this->load->view('template/head', $data);
+        $this->load->view('template/sidebar', $data);
+        $this->load->view('template/navbar', $data);
         $this->load->view('User/index', $data);
+        $this->load->view('template/foot');
+        
         
     }
 }
