d9fa53b27c410749886579756f7ad82ebf19b29a
unknown <ranidyaputri02@gmail.com>
2021-11-26 14:32:23 +0700

    memisahkan navbar pada source code

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -1,3 +1,17 @@
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
 
